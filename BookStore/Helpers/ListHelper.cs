b5b1a5eacb1dc304c1c07d4c954b9cda09338090
Dmytro <shkliar.dmytro@gmail.com>
2018-06-22 08:17:19 +0300

    HtmlHelpers

--- BookStore/Helpers/ListHelper.cs
+++ BookStore/Helpers/ListHelper.cs
@@ -8,7 +8,7 @@ namespace BookStore.Helpers
 {
     public static class ListHelper
     {
-        public static MvcHtmlString CreateList(this HtmlHelper html, string[] items)
+        /*public static MvcHtmlString CreateList(this HtmlHelper html, string[] items)
         {
             TagBuilder ul = new TagBuilder("ul");
             foreach (string item in items)
@@ -18,6 +18,19 @@ namespace BookStore.Helpers
                 ul.InnerHtml += li.ToString();
             }
             return new MvcHtmlString(ul.ToString());
+        }*/
+        public static MvcHtmlString CreateList(this HtmlHelper html, string[] items, object htmlAttributes = null)
+        {
+            TagBuilder ul = new TagBuilder("ul");
+            foreach (string item in items)
+            {
+                TagBuilder li = new TagBuilder("li");
+                li.SetInnerText(item);
+                ul.InnerHtml += li.ToString();
+            }
+            ul.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+
+            return MvcHtmlString.Create(ul.ToString());
         }
     }
 }
